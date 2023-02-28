7ce8300f9997b2a3ad0f2dca09172f31c28405c4
Keitoto <keitasekihara@gmail.com>
2023-02-28 17:39:01 +0900

    🎨 Stored API key to env

--- src/libs/client.ts
+++ src/libs/client.ts
@@ -3,5 +3,5 @@ import { createClient } from 'microcms-js-sdk'; //ES6
 // Initialize Client SDK.
 export const client = createClient({
   serviceDomain: 'shimabu-blog', // YOUR_DOMAIN is the XXXX part of XXXX.microcms.io
-  apiKey: 'SDdcGyZxSQlbBTE0vryx8uadaIZdRaoBM0mM',
+  apiKey: process.env.API_KEY,
 });
